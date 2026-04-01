84e5cba9befed114fc731dc9c4a4962d7f766d87
sacckey <sackeyad24@gmail.com>
2026-04-01 22:42:44 +0900

    feat: restructure site and demo files

--- scripts/update_demo_bundle.sh
+++ scripts/update_demo_bundle.sh
@@ -6,8 +6,6 @@ cd "$ROOT_DIR"
 
 moon build --target js src/cmd/web
 
-mkdir -p demo
-cp _build/js/debug/build/cmd/web/web.js demo/web.js
-cp _build/js/debug/build/cmd/web/web.js.map demo/web.js.map
+cp _build/js/debug/build/cmd/web/web.js site/demo/web.js
 
-echo "Updated demo/web.js and demo/web.js.map"
+echo "Updated site/demo/web.js"
